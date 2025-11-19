e47443e29c7765f8d2bd5c0849eeffa22c94f82e
jonaslefdal <jonas.lefdal@gmail.com>
2025-11-19 01:18:23 +0100

    f

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import cs2inspect
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Sticker(BaseModel):
     slot: int
